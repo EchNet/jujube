9a0d2223c1bd572ca9f4697978e5a6b820374db7
James Echmalian <ech@ech.net>
2012-11-16 14:48:42 -0500

    drop mode configuration handling

--- start.sh
+++ start.sh
@@ -4,33 +4,11 @@ cd "$( dirname "$0" )"
 
 ARG=${1-dev}
 
-# Argument determines mode.
-MODE=$ARG
-if [ "$ARG" = "verify" ] ; then
-    MODE=prod
-fi
-
 # Argument determines listener port.
 PORT=8001
 if [ "$ARG" = "test" ] || [ "$ARG" = verify ] ; then
 	PORT=8002
 fi
-
-# Mode determines configuration.
-if [ "$MODE" = "dev" ] ; then
-    CONFIG="config/cbase.json;config/dev.json"
-elif [ "$MODE" = "prod" ] ; then
-    CONFIG="config/cbase.json;config/prod.json"
-elif [ "$MODE" = "test" ] ; then
-    CONFIG="config/cbase.json;config/test.json"
-elif [ "$MODE" = "custom" ] ; then
-    CONFIG=$2
-else
-	echo "$MODE: unrecognized mode"
-	exit 1
-fi
-
-echo "Starting $MODE mode: $CONFIG"
 
 mvn -Dhub.config=${CONFIG} -Dhub.port=${PORT} jetty:run > hub.out 2>&1 &
 

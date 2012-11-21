5ae7eaaf80137592c4ab1a9d390ab186f5d58367
James Echmalian <ech@ech.net>
2012-11-21 16:04:03 -0500

    round out MongoDatabase tests

--- src/test/java/net/ech/mongo/MongoDatabaseTest.java
+++ src/test/java/net/ech/mongo/MongoDatabaseTest.java
@@ -8,10 +8,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 public class MongoDatabaseTest
 {
+	public final static String GOOD_URI = "mongodb://localhost:27027/test";
+
+	public MongoDatabaseTest() throws Exception
+	{
+		Runtime.getRuntime().exec("./test_mongo.sh");
+	}
+
     @Test
     public void testConstructMinimal() throws Exception
     {
@@ -22,51 +30,36 @@ public class MongoDatabaseTest
 	}
 
     @Test
-    public void testFailedConnection() throws Exception
+    public void testBadHostName() throws Exception
     {
-		MongoDatabase.Config config = new MongoDatabase.Config();
-		config.setUri("mongodb://nosuch:12/test");
-		MongoDatabase db = new MongoDatabase(config);
+		MongoDatabase db = new MongoDatabase(new MongoDatabase.Config("mongodb://nosuch/test"));
 		try {
-			db.withCollection("test").act(new MongoCollectionAction() {
-				public void act(DBCollection collection) {}
-			});
+			db.withCollection("test").act(null);
 			fail("should not be reached");
 		}
-		catch (IOException e) {
-			assertEquals("", e.getMessage());
+		catch (java.net.UnknownHostException e) {
+			assertEquals("nosuch", e.getMessage());
 		}
 	}
 
     @Test
-    public void testFailedAuthentication() throws Exception
+    public void testToStringOmitsQueryParams() throws Exception
     {
 		MongoDatabase.Config config = new MongoDatabase.Config();
-		config.setUri("mongodb://ech1:foyfoy@localhost:12/test");
+		config.setUri(GOOD_URI + "?connectTimeoutMS=100");
 		MongoDatabase db = new MongoDatabase(config);
-		try {
-			db.withCollection("test").act(new MongoCollectionAction() {
-				public void act(DBCollection collection) {}
-			});
-			fail("should not be reached");
-		}
-		catch (IOException e) {
-			assertEquals("", e.getMessage());
-		}
+		assertEquals(GOOD_URI, db.toString());
 	}
 
     @Test
-    public void testConstructAutoConnectRetry() throws Exception
+    public void testMongoOptionsMakeItThru() throws Exception
     {
-		MongoDatabase.Config config = new MongoDatabase.Config();
-		config.setUri("mongodb://localhost/test");
-		config.setAutoConnectRetry(true);
+		MongoDatabase.Config config = new MongoDatabase.Config(GOOD_URI + "?connectTimeoutMS=100");
 		MongoDatabase db = new MongoDatabase(config);
-		assertEquals("mongodb://localhost/test", db.toString());
 		db.withCollection("test").act(new MongoCollectionAction() {
 			@Override
 			public void act(DBCollection collection) {
-				assertTrue(collection.getDB().getMongo().getMongoOptions().isAutoConnectRetry());
+				assertEquals(100, collection.getDB().getMongo().getMongoOptions().getConnectTimeout());
 			}
 		});
 	}
@@ -74,14 +67,73 @@ public class MongoDatabaseTest
 	@Test
 	public void testConstructNoDB() throws Exception
 	{
-		MongoDatabase.Config config = new MongoDatabase.Config();
-		config.setUri("mongodb://localhost");
 		try {
-			new MongoDatabase(config);
+			new MongoDatabase(new MongoDatabase.Config("mongodb://localhost"));
 			fail("should not be reached");
 		}
 		catch (IOException e) {
 			assertEquals("no DB specified", e.getMessage());
 		}
+	}
+
+	@Test
+	public void testStaysOpen() throws Exception
+	{
+		MongoDatabase.Config config = new MongoDatabase.Config(GOOD_URI);
+		MongoDatabase db = new MongoDatabase(config);
+		assertFalse(db.isOpen());
+		db.withCollection("pouf").act(new MongoCollectionAction() {
+			@Override
+			public void act(DBCollection collection) {
+				collection.remove(new BasicDBObject());
+			}
+		});
+		assertTrue(db.isOpen());
+		db.withCollection("pouf").act(new MongoCollectionAction() {
+			@Override
+			public void act(DBCollection collection) {
+				collection.save(new BasicDBObject("msg", "hello"));
+			}
+		});
+		assertTrue(db.isOpen());
+		db.withCollection("pouf").act(new MongoCollectionAction() {
+			@Override
+			public void act(DBCollection collection) {
+				DBObject obj = collection.findOne(new BasicDBObject());
+				assertNotNull(obj);
+				assertEquals("hello", obj.get("msg"));
+			}
+		});
+		assertTrue(db.isOpen());
+	}
+
+	@Test
+	public void testGetsClosed() throws Exception
+	{
+		MongoDatabase.Config config = new MongoDatabase.Config(GOOD_URI);
+		config.setKeepAliveMillis(1);
+		final MongoDatabase db = new MongoDatabase(config);
+		db.withCollection("pouf").act(new MongoCollectionAction() {
+			@Override
+			public void act(DBCollection collection) throws IOException {
+				db.withCollection("pouf").act(new MongoCollectionAction() {
+					@Override
+					public void act(DBCollection collection) throws IOException {
+						db.withCollection("pouf").act(new MongoCollectionAction() {
+							@Override
+							public void act(DBCollection collection) throws IOException {
+							}
+						});
+						db.withCollection("pouf").act(new MongoCollectionAction() {
+							@Override
+							public void act(DBCollection collection) throws IOException {
+							}
+						});
+					}
+				});
+			}
+		});
+		Thread.sleep(100);
+		assertFalse(db.isOpen());
 	}
 }

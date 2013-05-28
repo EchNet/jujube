57feb34030812ad66def79bef3a85711d196a7e7
James Echmalian <ech@ech.net>
2013-05-28 15:25:38 -0400

    bump version

--- swoop_install.sh
+++ swoop_install.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mvn deploy:deploy-file -DgroupId=net.ech -DartifactId=jujube -Dversion=0.1 -DpomFile=pom.xml -Dpackaging=jar -Dfile=target/jujube-0.1.jar -Durl=dav:https://repository-swoop.forge.cloudbees.com/release/ -DrepositoryId=swoop-release
+VERSION=${1:-0.1.1}
+mvn deploy:deploy-file -DgroupId=net.ech -DartifactId=jujube -Dversion=$VERSION -DpomFile=pom.xml -Dpackaging=jar -Dfile=target/jujube-$VERSION.jar -Durl=dav:https://repository-swoop.forge.cloudbees.com/release/ -DrepositoryId=swoop-release
